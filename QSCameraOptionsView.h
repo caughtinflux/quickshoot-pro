054555176fc9f71a7483cc2efeaeb9c6a74e142b
caughtinflux <caughtinflux@icloud.com>
2013-03-12 12:58:14 +0530

    Add QSCameraOptionsView stub

---
+++ QSCameraOptionsView.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+#import "QSDefines.h"
+
+@interface QSCameraOptionsView : UIView
+
+@end
---
+++ QSCameraOptionsView.m
@@ -0,0 +1,5 @@
+#import "QSCameraOptionsView.h"
+
+@implementation QSCameraOptionsView
+
+@end
