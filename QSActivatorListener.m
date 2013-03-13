bd03324bb813120c97f2e35020ce596d0c163cef
caughtinflux <caughtinflux@icloud.com>
2013-03-13 17:40:16 +0530

    Write changes in the listener to prefs

--- QSActivatorListener.m
+++ QSActivatorListener.m
@@ -8,6 +8,7 @@
 static NSString * const QSCaptureListenerName = @"com.caughtinflux.quickshootpro.capturelistener";
 static NSString * const QSOptionsListenerName = @"com.caughtinflux.quickshootpro.optionslistener";
 
+
 @implementation QSActivatorListener
 {
     QSCameraOptionsWindow *_optionsWindow;
@@ -15,8 +16,6 @@ static NSString * const QSOptionsListenerName = @"com.caughtinflux.quickshootpro
 
 - (void)activator:(LAActivator *)activator receiveEvent:(LAEvent *)event
 {
-    DLog(@"%@", event);
-    DLog(@"%@", [[LAActivator sharedInstance] assignedListenerNameForEvent:event]);
     if ([[[LAActivator sharedInstance] assignedListenerNameForEvent:event] isEqualToString:QSCaptureListenerName]) {
         [[QSCameraController sharedInstance] takePhotoWithCompletionHandler:^(BOOL success) {
             ;// do nothing
@@ -37,20 +36,30 @@ static NSString * const QSOptionsListenerName = @"com.caughtinflux.quickshootpro
 
 - (void)optionsWindowCameraButtonToggled:(QSCameraOptionsWindow *)optionsWindow
 {
-    DLog(@"Toggling camera device. Current device: %i", [QSCameraController sharedInstance].cameraDevice);
     QSCameraDevice currentDevice = [[QSCameraController sharedInstance] cameraDevice];
     [[QSCameraController sharedInstance] setCameraDevice:((currentDevice == QSCameraDeviceRear) ? QSCameraDeviceFront : QSCameraDeviceRear)];
-    DLog(@"New camera device: %i", [QSCameraController sharedInstance].cameraDevice)
+
+    NSMutableDictionary *prefsDict = [NSMutableDictionary dictionaryWithContentsOfFile:kPrefPath];
+    prefsDict[QSCameraDeviceKey] = QSStringFromCameraDevice([QSCameraController sharedInstance].cameraDevice);
+    [prefsDict writeToFile:kPrefPath atomically:YES];
 }
 
 - (void)optionsWindow:(QSCameraOptionsWindow *)optionsWindow hdrModeChanged:(BOOL)newMode
 {
     [[QSCameraController sharedInstance] setEnableHDR:newMode];
+    
+    NSMutableDictionary *prefsDict = [NSMutableDictionary dictionaryWithContentsOfFile:kPrefPath];
+    prefsDict[QSHDRModeKey] = [NSNumber numberWithBool:newMode];
+    [prefsDict writeToFile:kPrefPath atomically:YES];
 }
 
 - (void)optionsWindow:(QSCameraOptionsWindow *)optionsWindow flashModeChanged:(QSFlashMode)newMode
 {
     [[QSCameraController sharedInstance] setFlashMode:newMode];
+
+    NSMutableDictionary *prefsDict = [NSMutableDictionary dictionaryWithContentsOfFile:kPrefPath];
+    prefsDict[QSFlashModeKey] = QSStringFromFlashMode(newMode);
+    [prefsDict writeToFile:kPrefPath atomically:YES];
 }
 
 @end
