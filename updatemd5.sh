2f01fc487a833ccd2b2cda390ee5207c029ec2a9
caughtinflux <caughtinflux@icloud.com>
2013-03-27 01:45:07 +0530

    Prettify updatemd5.sh output

--- updatemd5.sh
+++ updatemd5.sh
@@ -15,13 +15,19 @@ else
 	filepath="/Users/aditya/code/projects/quickshootpro/debs/obj/QuickShootPro.dylib"
 fi
 
+echo ""
 echo "md5'ing dylib"
+echo ""
 # cut up to the first space, removing the file name.
 echo `md5sum $filepath | cut -d ' ' -f 1` > /Users/aditya/Desktop/quickshoot_md5
 
+echo ""
 echo "Pushing to server!"
+echo ""
 scp /Users/aditya/Desktop/quickshoot_md5 flux@caughtinflux.com:/var/www/QuickShootPro/capabilities_sha1
 
+echo ""
 echo "Done!"
+echo ""
 
 exit 0
