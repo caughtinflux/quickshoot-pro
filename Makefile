52ca0d882d8bd6da4f79ee90746f650cb894afd8
caughtinflux <caughtinflux@icloud.com>
2013-03-27 01:22:06 +0530

    Add nice kQSVersion auto updating python script

--- Makefile
+++ Makefile
@@ -14,8 +14,8 @@ include $(THEOS_MAKE_PATH)/tweak.mk
 SUBPROJECTS += qsprefs
 include $(THEOS_MAKE_PATH)/aggregate.mk
 
+before-all::
+	-$(ECHO_NOTHING)./updateversion.py$(ECHO_END)
+
 before-install::
 	-$(ECHO_NOTHING)./updatemd5.sh$(ECHO_END)
-
-after-install::
-	$(ECHO_NOTHING)echo "Set correct kQSVersion!"$(ECHO_END)
--- updatemd5.sh
+++ updatemd5.sh
@@ -3,12 +3,25 @@
 if [ -z "$DEBUG" ]
 then
     filepath="/Users/aditya/code/projects/quickshootpro/debs/obj/debug/QuickShootPro.dylib"
+    echo ""
+    echo ""
+    echo ""
+    echo "In DEBUG mode, not bothering with MD5 Shit!"
+    echo ""
+    echo ""
+    echo ""
+    exit 0
 else
 	filepath="/Users/aditya/code/projects/quickshootpro/debs/obj/QuickShootPro.dylib"
 fi
 
 echo "md5'ing dylib"
+# cut up to the first space, removing the file name.
 echo `md5sum $filepath | cut -d ' ' -f 1` > /Users/aditya/Desktop/quickshoot_md5
 
 echo "Pushing to server!"
 scp /Users/aditya/Desktop/quickshoot_md5 flux@caughtinflux.com:/var/www/QuickShootPro/capabilities_sha1
+
+echo "Done!"
+
+exit 0
